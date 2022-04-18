db1d844588beb4b20d35407091fadf802caa4308
Muhammad Samiullah <msami398@gmail.com>
2022-04-18 14:58:52 +0500

    added Header and Footer for the dashboard

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <span>@Copyright from 2017-2022</span>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => (
+  <div className="header">
+    <h1>Dashboard</h1>
+    <ul>
+      <li>Tab1</li>
+      <li>Tab2</li>
+      <li>Signout</li>
+    </ul>
+  </div>
+);
+
+export default Header;
